c3e05a46b8886cf8a5e7307cf39a9060fc48dd35
isleeden <nkoreanarmy@gmail.com>
2021-02-26 22:32:12 +0500

    feat: implement transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  const result = [];
+
+  arr.forEach((item, index, array) => {
+    if (
+      (array[index - 1] === "--discard-next" &&
+        array[index + 1] === "--double-prev") ||
+      (item === "--double-prev" && array[index - 2] === "--discard-next")
+    ) {
+      return;
+    }
+    if (item === "--discard-next" || array[index - 1] === "--discard-next") {
+      return;
+    } else if (item === "--discard-prev") {
+      if (array[index - 1]) {
+        result.splice(index - 1, 1);
+      }
+    } else if (item === "--double-next") {
+      if (array[index + 1]) {
+        result.push(array[index + 1]);
+      }
+    } else if (item === "--double-prev") {
+      if (array[index - 1]) {
+        result.push(array[index - 1]);
+      }
+    } else {
+      result.push(item);
+    }
+  });
+
+  return result;
 };
