eeef8787da4408cf3144ee93a195c246f4491271
isleeden <nkoreanarmy@gmail.com>
2021-02-26 23:26:04 +0500

    feat: implement simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value === undefined) {
+      this.chain.push("( )");
+    } else {
+      this.chain.push("( " + value + " )");
+    }
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (this.chain[position - 1]) {
+      this.chain.splice(position - 1, 1);
+      return this;
+    } else {
+      this.chain = [];
+      throw "THROWN";
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let chain = this.chain;
+    this.chain = [];
+    return chain.join("~~");
+  },
 };
 
 module.exports = chainMaker;
