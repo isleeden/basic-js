6da0dd04c58eac6d922206a83b76c51073c65cc7
isleeden <nkoreanarmy@gmail.com>
2021-02-28 08:19:31 +0500

    feat: implement extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(
+  str,
+  { repeatTimes, separator, addition, additionRepeatTimes, additionSeparator }
+) {
+  if (addition === null) {
+    addition = "null";
+  }
+
+  if (str === null) {
+    str = "null";
+  }
+
+  separator = separator || "+";
+  additionSeparator = additionSeparator || "|";
+
+  const result = [];
+  let additionTemplate = [addition];
+
+  for (let i = 1; i < additionRepeatTimes; i++) {
+    additionTemplate.push(addition);
+  }
+
+  if (additionSeparator) {
+    additionTemplate = additionTemplate.join(additionSeparator);
+  } else {
+  }
+
+  result.push(str + additionTemplate);
+
+  for (let i = 1; i < repeatTimes; i++) {
+    result.push(str + additionTemplate);
+  }
+
+  return result.join(separator);
 };
-  
