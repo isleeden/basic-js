4956b2390f0d563edece87f2f1052adfc70ae871
isleeden <nkoreanarmy@gmail.com>
2021-03-01 20:08:12 +0500

    fix: fix transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,28 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(arr) {
-  const result = [];
+module.exports = function transform(argArr) {
+  const arr = argArr.slice(),
+    result = [];
 
   arr.forEach((item, index, array) => {
     if (
-      (array[index - 1] === "--discard-next" &&
-        array[index + 1] === "--double-prev") ||
-      (item === "--double-prev" && array[index - 2] === "--discard-next")
+      array[index + 1] === "--discard-prev" ||
+      item === "--discard-next" ||
+      item === "--discard-prev"
     ) {
       return;
-    }
-    if (item === "--discard-next" || array[index - 1] === "--discard-next") {
-      return;
-    } else if (item === "--discard-prev") {
-      if (array[index - 1]) {
-        result.splice(index - 1, 1);
-      }
+    } else if (array[index - 1] === "--discard-next") {
+      array[index] = undefined;
     } else if (item === "--double-next") {
-      if (array[index + 1]) {
+      if (array[index + 1] != undefined) {
         result.push(array[index + 1]);
       }
     } else if (item === "--double-prev") {
-      if (array[index - 1]) {
+      if (array[index - 1] != undefined) {
         result.push(array[index - 1]);
       }
     } else {
